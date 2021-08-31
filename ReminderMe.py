378250600aca70a869b71d7ddd243c746d15ff2e
arysuri23 <59773116+arysuri23@users.noreply.github.com>
2021-08-31 15:41:24 -0400

    Update ReminderMe.py

--- ReminderMe.py
+++ ReminderMe.py
@@ -32,18 +32,12 @@ while(a == "Y"):
     a = input("Would you like to enter another assignment? Enter Y or N?")
 
 print(assignments1)
-'''
-
+''' 
 assignments = {
-    'Psych Chapter One' : date(2021, 8, 29),
-    'CS 3330 Quiz' : date(2021, 9, 1),
-    'Stat 3080 project' : date(2021, 9, 3),
-    'Technosonics compo': date(2021, 9, 18),
-    'Stat 2120 HW' : date(2021, 10, 10)
-
-    #follow format to continue to add more
-
+    'CS Project' : date(9,23,2021)   #enter assignment with key being a string for the name of the assignment, and value as a date object
 }
+
+
 
 msg = "" 
 
@@ -74,8 +68,8 @@ smtp_server = "smtp.gmail.com"
 sender_email = "aryscodingemail@gmail.com"  # Enter your address
 receiver_email = "arysuri23@gmail.com"  # Enter receiver address
 password = "Watson123"                  # Enter your email password
-message = """
-Subject: Reminder
+message = """ Subject: Reminder
+
 """
 
 message += msg
@@ -85,62 +79,9 @@ with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.sendmail(sender_email, receiver_email, message)
 
 
-print('Done')
+print('Sent!)
 
 
 
 
 
-
-#from email import message
-#import smtplib, ssl
-#from email.mime.multipart import MIMEMultipart
-#from email.mime.text import MIMEText
-
-#to create html template for the email content
-#from string import Template
-
-#if we want images
-#import base64
-
-#Build email using html
-
-#message = """
- #       <!DOCTYPE html>
- #          <html>
-  #          <head>
-   #           <title>Hello World !</title>
-    #        </head>
-     #      <footer style="background-color: gray;">
-      #        <p style="text-align: center;">Contact us on : 
-       #                      <a href="mailto:haaply.apps@gmail.com">
-        #                     haaply.apps@gmail.com</a></p>
-         #     </footer>
- #   </body>
-  #</html>
-#"""
-
-
-# password = gmdjrbxtecskephs
-
-#connect to the SMTP server
-#s = smtplib.SMTP(host='smpt.gmail.com', port=587)
-#s.starttls()
-
-##login(sender email, sender password)
-#.login('ahs8gup@virginia.edu', 'gmdjrbxtecskephs')
-
-##msg = MIMEMultipart()
-
-#setup paramaters of msg
-#msg['From'] = 'ahs8gup@virginia.edu'
-#msg['To'] = 'ahs8gup@virginia.edu'
-#msg['Subject'] = 'Test'
-#msg.attach(MIMEText(message, 'html'))
-
-#send email
-##s.send_message(msg)
-#l msg
-
-#s.quit()
-#print('email sent')
